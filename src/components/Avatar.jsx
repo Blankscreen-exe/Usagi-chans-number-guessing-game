96f60a289bb41d54e885007743def6c326595828
Angel Henriquez <angelhm20@hotmail.com>
2025-04-25 15:45:39 -0400

    Finish responsive website

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -60,15 +60,15 @@ export default function Avatar(props) {
     }
 
     return (
-        <div className="flex flex-col justify-center items-center pt-2.5 gap-y-3 md:flex-row">
-            <span className="flex flex-col items-center w-full relative text-center z-0 text-[#666666] md:inline md:w-[480px]">
+        <div className="flex flex-col justify-center items-center pt-2.5 gap-y-3 md:gap-y-0 md:flex-row md:items-start">
+            <span className="flex flex-col items-center w-full relative text-center z-0 text-[#666666] md:inline md:w-[480px] lg:w-[600px]">
                 <picture>
                     <source media="(min-width: 768px)" srcSet="/speechbubble.png" />
-                    <img src="/speechbubble-mobile.png" alt="Speech buble" className="w-full h-72 max-w-80 md:h-56 md:max-w-none" />
+                    <img src="/speechbubble-mobile.png" alt="Speech buble" className="w-full h-72 max-w-80 md:h-56 md:max-w-none lg:h-auto" />
                 </picture>
-                <p className="absolute text-[.8rem] leading-6 max-w-72 top-0 mt-8 ml-auto mr-auto pl-6 pr-4 z-[1] md:max-w-[430px] md:mt-6 md:pl-8 md:text-[1rem]">{dialogue}</p>
+                <p className="absolute text-[.8rem] leading-6 max-w-72 top-0 mt-8 ml-auto mr-auto pl-6 pr-4 z-[1] md:max-w-[430px] md:mt-6 md:pl-8 md:text-[1rem] lg:max-w-[550px]">{dialogue}</p>
             </span>
-            <img src={image} className="w-36 md:w-50" />
+            <img src={image} />
         </div>
     )
 }
