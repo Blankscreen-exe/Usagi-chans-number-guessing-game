f7e82467c4d969efbac97f7bf0cdf9b4d2de6b0a
Angel Henriquez <angelhm20@hotmail.com>
2025-04-24 20:47:17 -0400

    IOBox responsive for mobile

--- src/components/AnswerButtons.jsx
+++ src/components/AnswerButtons.jsx
@@ -21,10 +21,10 @@ export default function AnswerButtons(props) {
     }
 
     return (
-        <div className="flex flex-col items-center gap-y-5">
+        <div className="flex flex-col items-center gap-y-3.5 md:gap-y-5">
             <p>Current Number: <span className="text-orange-600 font-bold underline">{props.numbers.currentNumber}</span></p>
             <p>The next number is going to be ...</p>
-            <div className="flex justify-between w-[400px] m-auto">
+            <div className="flex justify-between flex-col gap-y-4 md:w-[400px] md:m-auto">
                 <button className="answer-button border-4 border-[#e12407] btn-shadow py-2.5 px-5 text-[15pt] font-bold font-[yoster] bg-[#ffd8d2] cursor-pointer text-[#e12407] hover:bg-[#ff7a65] hover:border-[#d42a10] hover:text-white focus:outline-0 focus-visible:outline-0" onClick={handleAnswer} name="smaller">Smaller 🔻</button>
                 <button className="answer-button border-4 border-[#e12407] btn-shadow py-2.5 px-5 text-[15pt] font-bold font-[yoster] bg-[#ffd8d2] cursor-pointer text-[#e12407] hover:bg-[#ff7a65] hover:border-[#d42a10] hover:text-white focus:outline-0 focus-visible:outline-0" onClick={handleAnswer} name="larger">Larger 🔺</button>
             </div>
