874b1f701b68a9b3d14061de2298f385add008e6
Angel Henriquez <angelhm20@hotmail.com>
2025-04-24 20:15:45 -0400

    nav buttons modal responsive

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,9 +9,9 @@ export default function Dashboard(props) {
         <Popup trigger={<button className='border-4 border-[#e12407] btn-shadow py-3 px-5 text-[15pt] font-bold font-[yoster] bg-[#ffd8d2] cursor-pointer text-[#e12407] min-w-44 hover:bg-[#ff7a65] hover:border-[#d42a10] hover:text-white focus:outline-0 focus-visible:outline-0' >🥕 Roster</button>} modal >
             <div>
                 <div className="relative">
-                    <div className="absolute top-1/2 left-1/2 min-w-[800px] -translate-1/2 text-center">
+                    <div className="text-center px-6 max-w-[400px] md:min-w-[800px]">
                         <div className="text-white">
-                            <h2 className="text-[1.5em]">This section is currently under development. But nothing's stopping you from playing the game so why not have a go at it since you are here?</h2>
+                            <h2 className="text-[1.3em] md:text-[1.5em]">This section is currently under development. But nothing's stopping you from playing the game so why not have a go at it since you are here?</h2>
 
                         </div>
                     </div>
